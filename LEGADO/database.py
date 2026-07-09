daeda243533ba3b40f935a4e10a07c55391c2d9a
maaaattttiiii <mnvvargas3@gmail.com>
2026-07-09 15:53:26 -0300

    Update database.py

--- LEGADO/database.py
+++ LEGADO/database.py
@@ -80,6 +80,7 @@ def inicializar_sistema():
         cursor.close()
         conn.close()
 
+@st.cache_data(ttl=600)
 def obtener_arbitros():
     conn = conectar_db()
     if not conn: return {"-- Seleccionar --": None}
@@ -92,6 +93,7 @@ def obtener_arbitros():
     for arb in arbitros: dicc[arb['nombre_completo']] = arb['id_arbitro']
     return dicc
 
+@st.cache_data(ttl=600)
 def obtener_partidos_pendientes():
     conn = conectar_db()
     if not conn: return {"-- Seleccionar --": None}
